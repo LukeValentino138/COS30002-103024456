6bb59cdc5d26d3625d95e82169532ccae9cbf7ff
LukeValentino138 <110918027+LukeValentino138@users.noreply.github.com>
2024-05-01 19:21:45 +1000

    Changed heuristic
    changed heuristic

--- 05 - Lab - Graphs, Paths and Search/sample/box_world.py
+++ 05 - Lab - Graphs, Paths and Search/sample/box_world.py
@@ -318,8 +318,8 @@ class BoxWorld(object):
         self.path = None # invalid so remove if present
         self.graph = SparseGraph()
         # Set a heuristic cost function for the search to use
-        self.graph.cost_h = self._manhattan
-        #self.graph.cost_h = self._hypot
+        #self.graph.cost_h = self._manhattan
+        self.graph.cost_h = self._hypot
         #self.graph.cost_h = self._max
 
         nx, ny = self.nx, self.ny

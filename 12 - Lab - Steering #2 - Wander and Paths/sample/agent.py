e4c749180bb4643b4cdb1cde74e1e1824c10d5b6
LukeValentino138 <110918027+LukeValentino138@users.noreply.github.com>
2024-05-27 01:45:11 +1000

    Completed wander

--- 12 - Lab - Steering #2 - Wander and Paths/sample/agent.py
+++ 12 - Lab - Steering #2 - Wander and Paths/sample/agent.py
@@ -60,14 +60,20 @@ class Agent(object):
         self.randomise_path()
         self.waypoint_threshold = 40.0 
 
-        # limits?
+        # NEW WANDER INFO
+        self.wander_target = Vector2D(1, 0)
+        self.wander_dist = 1.0 * scale
+        self.wander_radius = 1.0 * scale
+        self.wander_jitter = 10.0 * scale
+        self.bRadius = scale
+        # Force and speed limiting code
         self.max_speed = 20.0 * scale
-        ## max_force ??
+        self.max_force = 500.0
 
         # debug draw info?
         self.show_info = False
 
-    def calculate(self):
+    def calculate(self, delta):
         # calculate the current steering force
         mode = self.mode
         if mode == 'seek':
@@ -82,8 +88,8 @@ class Agent(object):
             force = self.flee(self.world.target)
         elif mode == 'pursuit':
             force = self.pursuit(self.world.hunter)
-        # elif mode == 'wander':
-        #     force = self.wander(delta)
+        elif mode == 'wander':
+            force = self.wander(delta)
         elif mode == 'follow_path':
             force = self.follow_path()
         else:
@@ -95,7 +101,8 @@ class Agent(object):
         ''' update vehicle position and orientation '''
         # calculate and set self.force to be applied
         ## force = self.calculate()
-        force = self.calculate()  # <-- delta needed for wander
+        force = self.calculate(delta)  # <-- delta needed for wander
+        force.truncate(self.max_force)
         ## limit force? <-- for wander
         # ...
         # determin the new accelteration
@@ -145,6 +152,20 @@ class Agent(object):
             egi.white_pen()
             egi.line_with_arrow(self.pos+self.vel * s, self.pos+ (self.force+self.vel) * s, 5)
             egi.line_with_arrow(self.pos, self.pos+ (self.force+self.vel) * s, 5)
+
+
+        if self.mode == 'wander':
+            # calculate the center of the wander circle in front of the agent
+            wnd_pos = Vector2D(self.wander_dist, 0)
+            wld_pos = self.world.transform_point(wnd_pos, self.pos, self.heading, self.side)
+            # draw the wander circle
+            egi.green_pen()
+            egi.circle(wld_pos, self.wander_radius)
+            # draw the wander target (little circle on the big circle)
+            egi.red_pen()
+            wnd_pos = (self.wander_target + Vector2D(self.wander_dist, 0))
+            wld_pos = self.world.transform_point(wnd_pos, self.pos, self.heading, self.side)
+            egi.circle(wld_pos, 3)
 
     def speed(self):
         return self.vel.length()
@@ -209,3 +230,22 @@ class Agent(object):
             if self.pos.distanceSq(self.path.current_pt()) < self.waypoint_threshold ** 2:
                 self.path.inc_current_pt()
             return self.arrive(self.path.current_pt(), 'slow')
+    def wander(self, delta):
+        ''' random wandering using a projected jitter circle '''
+        wt = self.wander_target
+        # this behaviour is dependent on the update rate, so this line must
+        # be included when using time independent framerate.
+        jitter_tts = self.wander_jitter * delta # this time slice
+        # first, add a small random vector to the target's position
+        wt += Vector2D(uniform(-1,1) * jitter_tts, uniform(-1,1) * jitter_tts)
+        # re-project this new vector back on to a unit circle
+        wt.normalise()
+        # increase the length of the vector to the same as the radius
+        # of the wander circle
+        wt *= self.wander_radius
+        # move the target into a position WanderDist in front of the agent
+        target = wt + Vector2D(self.wander_dist, 0)
+        # project the target into world space
+        wld_target = self.world.transform_point(target, self.pos, self.heading, self.side)
+        # and steer towards it
+        return self.seek(wld_target)
